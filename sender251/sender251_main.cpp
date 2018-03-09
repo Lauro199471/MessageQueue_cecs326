7877a44e5ed03dba2ff1704c5a5425de30ce7f36
Anel Ruiz <anel.a.ruiz@gmail.com>
2018-03-08 18:16:29 -0800

    251 start

--- sender251/sender251_main.cpp
+++ sender251/sender251_main.cpp
@@ -4,6 +4,27 @@
 #include <signal.h>
 #include <unistd.h>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <cstring>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstdlib>
+#include <time.h>
+
+
+using namespace std;
+
+// Declare the message structure
+typedef struct msg_buf 
+{
+  long mtype;
+  char mtext[256] = {};
+} message_buffer;
+
+void sendMSG(message_buffer* , int , size_t ,int);
+void deadMSG(message_buffer* , int , size_t);
 
 void signalHandler(int signal)
 {
@@ -13,13 +34,20 @@ void signalHandler(int signal)
 
 int main()
 {
+    bool divisable;
+    int randomNumber;
+    printf("PID=%d\n",getpid());
+    srand(time(NULL));
     signal(SIGUSR1,signalHandler);
     
-    printf("PID=%d\n",getpid());
+
     while(1)
     {
-        std::cout << "I am john, and i like big culones and males ass " <<
-                getpid() << std::endl;
+    randomNumber = rand();
+    divisable = ((randomNumber % marker) == 0)? 1 : 0;
+
+       // std::cout << "I am john, and i like big culones and males ass " <<
+         //       getpid() << std::endl;
     }
     return 0;
 }
